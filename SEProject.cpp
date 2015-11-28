db9a3cc39cf6c73633125869ad6545e7b4126340
Teresa Shim <teresa2000@naver.com>
2015-11-29 00:40:42 +0900

    2nd
    경로계산보다는 로봇이 진행할 방향을 잡을 때 사용하는 정도로 생각하고 수정.
    2차원배열 dis에 해당 노드에서 목표지점까지의 최소 거리를 저장.
    맵 전체에 대해서 구하는 것은 낭비라고 생각하여 출발 지점과 목표지점을 맞 꼭지점으로 하는 직사각형 모양의 맵 안에서만 계산하도록 설정.

--- SEProject.cpp
+++ SEProject.cpp
@@ -26,7 +26,7 @@ protected:
 	bool IsDetected;
 };
 
-void InitDis()					//dis 초기화
+void InitDis()										//dis 초기화
 {
 	int i, j;
 	
@@ -37,18 +37,18 @@ void InitDis()					//dis 초기화
 	}
 }
 
-void CalPath(int x, int y)				//경로를 계산하는 부분.
+void CalPath(int x, int y)							//노드에서 목표지점까지의 거리를 계산하는 부분. 방향을 잡는 용도로만 쓰일 듯.
 {
 	int i, j;
 
-	if(map[x][y].kind==HAZ);			//hazard면 계산하지 않음.
-	else
+	if(map[x][y].kind==HAZ);						//hazard면 계산하지 않음.
+	else if((TAR_X<x)&&(TAR_Y<y))					//x, y를 기준으로 목표지점이 3사분면에 있을 때
 	{
-		for(i=min(x, TAR_X); i<max(x, TAR_X); i++)
+		for(i=TAR_X; i<x; i++)
 		{
-			for(j=min(y, TAR_Y); j<max(y, TAR_Y); j++)
+			for(j=TAR_Y; j<y; j++)
 			{
-				if((i==x)&&(j==y))					//시작점인 경우
+				if((i==TAR_X)&&(j==TAR_Y))			//시작점인 경우
 					continue;
 				if(dis[i+1][j]>dis[i][j]+1)			//오른쪽 검사
 				{
@@ -73,34 +73,124 @@ void CalPath(int x, int y)				//경로를 계산하는 부분.
 			}
 		}
 	}
-
-}
-
-void FindPath(int x, int y)					//경로를 저장하는 부분
-{
-	int i=TAR_X, j=TAR_Y;
-
-	while((i!=x)&&(j!=y))					//노드까지의 거리를 확인하며 도착지점부터 출발지점까지 경로를 리스트에 저장.
+	else if((TAR_X<x)&&(TAR_Y>y))					//x, y를 기준으로 목표지점이 2사분면에 있을 때
 	{
-		if(dis[i][j]==dis[i+1][j]+1)
+		for(i=TAR_X; i<x; i++)
 		{
-			path.push_front(*map[i+1][j]);
-			i++;
-		}
-		else if(dis[i][j]==dis[i-1][j]+1)
-		{
-			path.push_front(*map[i-1][j]);
-			i--;
-		}
-		else if(dis[i][j]==dis[i][j+1]+1)
-		{
-			path.push_front(*map[i][j+1]);
-			j++;
-		}
-		else if(dis[i][j]==dis[i][j-1]+1)
-		{
-			path.push_front(*map[i][j-1]);
-			j--;
+			for(j=TAR_Y; j>y; j--)
+			{
+				if((i==TAR_X)&&(j==TAR_Y))			//시작점인 경우
+					continue;
+				if(dis[i+1][j]>dis[i][j]+1)			//오른쪽 검사
+				{
+					dis[i+1][j]=dis[i][j]+1;
+					CalPath(i+1, j);
+				}
+				if(dis[i-1][j]>dis[i][j]+1)			//왼쪽 검사
+				{
+					dis[i-1][j]=dis[i][j]+1;
+					CalPath(i-1, j);
+				}
+				if(dis[i][j+1]>dis[i][j]+1)			//위쪽 검사
+				{
+					dis[i][j+1]=dis[i][j]+1;
+					CalPath(i, j+1);
+				}
+				if(dis[i][j-1]>dis[i][j]+1)			//밑쪽 검사
+				{
+					dis[i][j-1]=dis[i][j]+1;
+					CalPath(i, j-1);
+				}
+			}
 		}
 	}
+	else if((TAR_X>x)&&(TAR_Y<y))					//x, y를 기준으로 목표지점이 4사분면에 있을 때
+	{
+		for(i=TAR_X; i>x; i--)
+		{
+			for(j=TAR_Y; j<y; j++)
+			{
+				if((i==TAR_X)&&(j==TAR_Y))			//시작점인 경우
+					continue;
+				if(dis[i+1][j]>dis[i][j]+1)			//오른쪽 검사
+				{
+					dis[i+1][j]=dis[i][j]+1;
+					CalPath(i+1, j);
+				}
+				if(dis[i-1][j]>dis[i][j]+1)			//왼쪽 검사
+				{
+					dis[i-1][j]=dis[i][j]+1;
+					CalPath(i-1, j);
+				}
+				if(dis[i][j+1]>dis[i][j]+1)			//위쪽 검사
+				{
+					dis[i][j+1]=dis[i][j]+1;
+					CalPath(i, j+1);
+				}
+				if(dis[i][j-1]>dis[i][j]+1)			//밑쪽 검사
+				{
+					dis[i][j-1]=dis[i][j]+1;
+					CalPath(i, j-1);
+				}
+			}
+		}
+	}
+	else if((TAR_X>x)&&(TAR_Y>y))					//x, y를 기준으로 목표지점이 1사분면에 있을 때
+	{
+		for(i=TAR_X; i>x; i--)
+		{
+			for(j=TAR_Y; j>y; j--)
+			{
+				if((i==TAR_X)&&(j==TAR_Y))			//시작점인 경우
+					continue;
+				if(dis[i+1][j]>dis[i][j]+1)			//오른쪽 검사
+				{
+					dis[i+1][j]=dis[i][j]+1;
+					CalPath(i+1, j);
+				}
+				if(dis[i-1][j]>dis[i][j]+1)			//왼쪽 검사
+				{
+					dis[i-1][j]=dis[i][j]+1;
+					CalPath(i-1, j);
+				}
+				if(dis[i][j+1]>dis[i][j]+1)			//위쪽 검사
+				{
+					dis[i][j+1]=dis[i][j]+1;
+					CalPath(i, j+1);
+				}
+				if(dis[i][j-1]>dis[i][j]+1)			//밑쪽 검사
+				{
+					dis[i][j-1]=dis[i][j]+1;
+					CalPath(i, j-1);
+				}
+			}
+		}
+	}
+	else if(x==TAR_X)								//x, y가 목표지점과 x축으로 일직선상에 놓여있을 때
+	{
+		if(y>TAR_Y)
+		{
+			for(j=y; j>TAR_Y; j--)					//x, y를 기준으로 목표지점이 아래에 있을 때
+				dis[x][j-1]=dis[x][j]+1;
+		}
+		else										//x, y를 기준으로 목표지점이 위에 있을 때
+		{
+			for(j=y; j<TAR_Y; j++)
+				dis[x][j+1]=dis[x][j]+1;
+		}
+	}
+	else if(y==TAR_Y)								//x, y가 목표지점과 y축으로 일직선상에 놓여있을 때
+	{
+		if(x>TAR_X)
+		{
+			for(i=x; i>TAR_X; i--)					//x, y를 기준으로 목표지점이 왼쪽에 있을 때
+				dis[i-1][y]=dis[i][y]+1;
+		}
+		else										//x, y를 기준으로 목표지점이 오른쪽에 있을 때
+		{
+			for(i=x; i<TAR_X; i++)
+				dis[i+1][y]=dis[i][y]+1;
+		}
+	}
+
 }
